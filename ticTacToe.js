7d7a760e0334144edd990e155978abbe0921ac74
bjcurriden <bjcurriden@gmail/.com>
2022-02-04 15:20:01 +0100

    lower case declared in only one place

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,9 @@
 /*ask for submission from user of either rock, paper, or scissors assign it to variable playerSelection
 	(convert it to all lower case on return so that it’s not case sensitive)*/
 
-let playerSelection = prompt("Pick rock, paper, or scissors");
-console.log(playerSelection.toLowerCase());
+let playerSelectionAny = prompt("Pick rock, paper, or scissors");
+let playerSelection = playerSelectionAny.toLowerCase();
+console.log(playerSelection);
 
 /*setup an array of [rock, paper, scissors]*/
 const cpuOptions = ["rock", "paper", "scissors"];
@@ -14,33 +15,33 @@ console.log(computerSelection);
 
 function playRound(computerSelection, playerSelection) {
 
-if ((computerSelection == "rock") && (playerSelection.toLowerCase() == "paper")) {
+if ((computerSelection == "rock") && (playerSelection == "paper")) {
 return "You win! Paper covers rock!"
 }
 
-if ((computerSelection == "rock") && (playerSelection.toLowerCase() == "scissors")) {
+if ((computerSelection == "rock") && (playerSelection == "scissors")) {
 return("You lose! Rock smashes scissors!");
 }
 
-if ((computerSelection == "paper") && (playerSelection.toLowerCase() == "scissors")) {
+if ((computerSelection == "paper") && (playerSelection == "scissors")) {
 return("You win! Scissors cut paper!");
 }
 
-if ((computerSelection == "paper") && (playerSelection.toLowerCase() == "rock")) {
+if ((computerSelection == "paper") && (playerSelection == "rock")) {
 return("You lose! Paper covers rock!");
 }
 
-if ((computerSelection == "scissors") && (playerSelection.toLowerCase() == "rock")) {
+if ((computerSelection == "scissors") && (playerSelection == "rock")) {
 return("You win! Rock smashes scissors!");
 }
 
-if ((computerSelection == "scissors") && (playerSelection.toLowerCase() == "paper")) {
+if ((computerSelection == "scissors") && (playerSelection == "paper")) {
 return("You lose! Scissors cut paper!");
 }
 
-if (computerSelection == playerSelection.toLowerCase()) {
+if (computerSelection == playerSelection) {
     return("it's a tie");
 }
 }
-
+/*alert the player of the results*/
 alert(playRound(computerSelection, playerSelection));
