fc18e6bfbc625f827df8f701c793e0ba23eae851
bjcurriden <bjcurriden@gmail/.com>
2022-02-17 20:26:30 +0100

    added alerts

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,9 +5,9 @@ let computerScore = 0;
    
 const playRound = (computerSelection, playerSelection) => {
 
-     /*ask for submission from user of either rock, paper, or scissors assign it to variable playerSelection
-            (convert it to all lower case on return so that it’s not case sensitive)*/
+//ask for submission from user of either rock, paper, or scissors assign it to variable playerSelection
 let playerSelectionAny = prompt("Pick rock, paper, or scissors");
+//(convert it to all lower case on return so that it’s not case sensitive)
 playerSelection = playerSelectionAny.toLowerCase();
 console.log(playerSelection);
         
@@ -20,43 +20,43 @@ console.log(computerSelection);
 
 
     if ((computerSelection == "rock") && (playerSelection == "paper")) {
-        console.log("You win! Paper covers rock!")
+        alert("You win! Paper covers rock!")
         return playerScore += 1;
         } 
 
     else if ((computerSelection == "rock") && (playerSelection == "scissors")) {
-        console.log("You lose! Rock smashes scissors!");
+        alert("You lose! Rock smashes scissors!");
         return computerScore += 1;
         }
 
     else if ((computerSelection == "paper") && (playerSelection == "scissors")) {
-        console.log("You win! Scissors cut paper!");
+        alert("You win! Scissors cut paper!");
         return playerScore += 1;
         }
 
     else if ((computerSelection == "paper") && (playerSelection == "rock")) {
-        console.log("You lose! Paper covers rock!");
+        alert("You lose! Paper covers rock!");
         return computerScore += 1;
         }
         
     else if ((computerSelection == "scissors") && (playerSelection == "rock")) {
-        console.log("You win! Rock smashes scissors!");
+        alert("You win! Rock smashes scissors!");
         return playerScore += 1;
         }
 
     else if ((computerSelection == "scissors") && (playerSelection == "paper")) {
-        console.log("You lose! Scissors cut paper!");
+        alert("You lose! Scissors cut paper!");
         return computerScore +=1
         }
         
 
     else if (computerSelection == playerSelection) {
-            console.log("it's a tie");
+            alert("it's a tie");
             return playerScore = playerScore
         }
 
     else {
-            console.log("pick a valid option, you nitwit!")
+            alert("pick a valid option, you nitwit!")
         }
 
         /*alert the player of the results*/
