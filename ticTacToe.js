965c78b5de0d0fe38f5c227c9eede5aff4054835
bjcurriden <bjcurriden@gmail/.com>
2022-02-17 19:59:58 +0100

    it works

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,51 +1,79 @@
 
-    /*ask for submission from user of either rock, paper, or scissors assign it to variable playerSelection
-        (convert it to all lower case on return so that it’s not case sensitive)*/
-    let playerSelectionAny = prompt("Pick rock, paper, or scissors");
-    let playerSelection = playerSelectionAny.toLowerCase();
-    console.log(playerSelection);
+let playerScore = 0;
+let computerScore = 0;
+   
+   
+const playRound = (computerSelection, playerSelection) => {
 
-    /*setup an array of [rock, paper, scissors]*/
-    const cpuOptions = ["rock", "paper", "scissors"];
+     /*ask for submission from user of either rock, paper, or scissors assign it to variable playerSelection
+            (convert it to all lower case on return so that it’s not case sensitive)*/
+let playerSelectionAny = prompt("Pick rock, paper, or scissors");
+playerSelection = playerSelectionAny.toLowerCase();
+console.log(playerSelection);
+        
+/*setup an array of [rock, paper, scissors]*/
+const cpuOptions = ["rock", "paper", "scissors"];
+        
+/*computer randomly chooses from array and assigns it to variable computerSelection*/
+computerSelection = cpuOptions[Math.floor(Math.random()*cpuOptions.length)];
+console.log(computerSelection);
 
-    /*computer randomly chooses from array and assigns it to variable computerSelection*/
-    computerSelection = cpuOptions[Math.floor(Math.random()*cpuOptions.length)];
-    console.log(computerSelection);
-
-
-    function playRound(computerSelection, playerSelection) {
 
     if ((computerSelection == "rock") && (playerSelection == "paper")) {
-    return "You win! Paper covers rock!"
-    }
+        console.log("You win! Paper covers rock!")
+        return playerScore += 1;
+        } 
 
-    if ((computerSelection == "rock") && (playerSelection == "scissors")) {
-    return("You lose! Rock smashes scissors!");
-    }
+    else if ((computerSelection == "rock") && (playerSelection == "scissors")) {
+        console.log("You lose! Rock smashes scissors!");
+        return computerScore += 1;
+        }
 
-    if ((computerSelection == "paper") && (playerSelection == "scissors")) {
-    return("You win! Scissors cut paper!");
-    }
+    else if ((computerSelection == "paper") && (playerSelection == "scissors")) {
+        console.log("You win! Scissors cut paper!");
+        return playerScore += 1;
+        }
 
-    if ((computerSelection == "paper") && (playerSelection == "rock")) {
-    return("You lose! Paper covers rock!");
-    }
+    else if ((computerSelection == "paper") && (playerSelection == "rock")) {
+        console.log("You lose! Paper covers rock!");
+        return computerScore += 1;
+        }
+        
+    else if ((computerSelection == "scissors") && (playerSelection == "rock")) {
+        console.log("You win! Rock smashes scissors!");
+        return playerScore += 1;
+        }
 
-    if ((computerSelection == "scissors") && (playerSelection == "rock")) {
-    return("You win! Rock smashes scissors!");
-    }
+    else if ((computerSelection == "scissors") && (playerSelection == "paper")) {
+        console.log("You lose! Scissors cut paper!");
+        return computerScore +=1
+        }
+        
 
-    if ((computerSelection == "scissors") && (playerSelection == "paper")) {
-    return("You lose! Scissors cut paper!");
-    }
+    else if (computerSelection == playerSelection) {
+            console.log("it's a tie");
+            return playerScore = playerScore
+        }
 
-    if (computerSelection == playerSelection) {
-        return("it's a tie");
-    }
+    else {
+            console.log("pick a valid option, you nitwit!")
+        }
 
-    else if (computerSelection !== playerSelection) {
-        return("pick a valid option, you nitwit!")
+        /*alert the player of the results*/
+        alert(playRound(computerSelection, playerSelection)); 
+        console.log(playerScore, computerScore);
+
+        }
+     
+    
+     
+        
+const playGame = () => {
+    for (let i=0; i<5; i++) {
+        playRound();
     }
-    }
-    /*alert the player of the results*/
-    alert(playRound(computerSelection, playerSelection));
+    alert(playerScore + ":" + computerScore);
+}   
+
+playGame();
+
